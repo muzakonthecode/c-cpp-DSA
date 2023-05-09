6662cb4edc8241a266bb0178f9cd6c17c1ea0d32
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-05-09 18:42:18 +0300

    first commit

---
+++ array_insert.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int arr[100], n, elem, pos;
+
+void insert()
+{
+    pos = pos - 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos] = elem;
+}
+
+int main()
+{
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the array's elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter an element to be inserted: ");
+    scanf("%d", &elem);
+
+    printf("Enter the position: ");
+    scanf("%d", &pos);
+
+    printf("Before: %d\n", arr[0]);
+    insert();
+    printf("After: %d\n", arr[0]);
+
+    return 0;
+}
+
+/*
+Before: 2
+After : 0
+*/
